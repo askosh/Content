22357116a60f6a1d37bbe741fa86dad68dd6ca75
Asko Nomm <asko.nomm@greenpowermonitor.com>
2019-08-28 12:43:30 +0300

    bump

--- Sources/Content/Content.swift
+++ Sources/Content/Content.swift
@@ -157,7 +157,7 @@ class Content {
       let status = yaml["status"] as! String
       let slug = yaml["slug"] as! String
       let date = yaml["date"] as! Date
-      let timeAgo = ContentUtils().relativeTime(datetime: date)
+      let timeAgo = self.relativeTime(datetime: date)
       let entry = item["entry"] as! String
 
       items.append(ContentItem(title: title, status: status, slug: slug, date: timeAgo, entry: entry))
@@ -185,6 +185,102 @@ class Content {
     }
 
     return item
+
+  }
+
+  /// This is merely a helper fn that returns a "s" when the `n` is more than 1
+  private func sStrOptional(n: Int) -> String {
+
+    if n > 1 {
+
+      return "s"
+
+    }
+
+    return ""
+
+  }
+
+  /**
+   Turns a timestamp string into a relative time.
+   
+   - Parameter datetime: Datetime string.
+   
+   - Returns: String.
+   */
+  private func relativeTime(datetime: Date) -> String {
+    
+    let currentTimestamp = Date().currentTimeMillis()
+    let timestamp = datetime.currentTimeMillis()
+    let difference: Int = currentTimestamp - timestamp
+    let minute: Int = 60 * 1000
+    let hour: Int = minute * 60
+    let day: Int = hour * 24
+    let month: Int = day * 30
+    let year: Int = day * 365
+    
+    if(difference < minute) {
+        
+      let s: String = self.sStrOptional(n: difference / 1000)
+      let seconds: String = String(difference / 1000)
+      
+      return seconds + " second" + s + " ago"
+        
+    } else if(difference < hour) {
+      
+      let s: String = self.sStrOptional(n: difference / minute)
+      let minutes: String = String(difference / minute)
+      
+      return minutes + " minute" + s + " ago"
+      
+    } else if(difference < day) {
+        
+      let s: String = self.sStrOptional(n: difference / hour)
+      let hours: String = String(difference / hour)
+      
+      return hours + " hour" + s + " ago"
+        
+    } else if(difference < month) {
+        
+      let s: String = self.sStrOptional(n: difference / day)
+      let days: String = String(difference / day)
+      
+      return days + " day" + s + " ago"
+        
+    } else if(difference < year) {
+      
+      let s: String = self.sStrOptional(n: difference / month)
+      let months: String = String(difference / month)
+      
+      return months + " month" + s + " ago"
+        
+    } else {
+        
+      return "Years"
+       
+    }
+      
+  }
+
+}
+
+extension Date {
+
+  // Get current Epoch time
+  func currentTimeMillis() -> Int {
+        
+    return Int(self.timeIntervalSince1970 * 1000)
+      
+  }
+
+  /// Convert Date to String, in a given `dateFormat`
+  func toString(dateFormat format: String) -> String {
+
+    let dateFormatter = DateFormatter()
+
+    dateFormatter.dateFormat = format
+
+    return dateFormatter.string(from: self)
 
   }
 
